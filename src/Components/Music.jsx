b11cf3ade9d9fa61eac80ef5b9eafcb4779a2937
PeterFiorentino <peterfiorentino@pursuit.org>
2020-09-09 20:45:55 -0400

    added new video to music

--- src/Components/Music.jsx
+++ src/Components/Music.jsx
@@ -14,6 +14,7 @@ class Music extends React.Component{
     render() {
         return (
             <div id="music">
+                <YouTube videoId = "SBHj0cTQZps" className="musicVideo"/>
                 <YouTube videoId = "hxPLwb8z7sI" className="musicVideo"/>
                 <YouTube videoId = "76PRw0s886w" className="musicVideo"/>
                 <YouTube videoId = "IGxhfyRWWX4" className="musicVideo"/>
