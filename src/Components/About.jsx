3551463839eaecc762d4bb887c285b1c97117537
PeterFiorentino <peterfiorentino@pursuit.org>
2020-10-16 00:02:54 -0400

    updating my bio in my about component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -25,11 +25,25 @@ class About extends React.Component{
             <div id="About">
                 <img src={suit} id="1" onMouseOver={this.changePic} className="aboutPic" alt="Pics of Peter"></img>    
                 <p>
-                    All my life I have loved music. I learned how to play saxaphone in high school and used that 
-                    knowledge to teach myself how to play other instruments including piano and bass. My passion for 
-                    music is actually how I found my skill in tech. I taught myself Digital Audio Workstations like Pro Tools
-                    and Ableton in order to record my music. Though it was a completely different field, having that background 
-                    in tech made my transition from musician to Software Engineer feel welcoming and familiar.
+                    Coding helped me discover the truth about a career.
+                    <br />
+                    <br /> 
+                    I am a self-taught musician - I release an original song and music video every month on my YouTube channel. When I graduated with a BA in English from Brooklyn College, I wrote a (not YET best-selling) book about growing up with anxiety. I work very hard at it, but I am naturally a creative person.
+                    <br />
+                    <br />
+                    I have always seen my creativity as a blessing. It pushes me towards innovative solutions to problems I come across daily. Unfortunately, I was not finding jobs that let me flex my creative muscle. After talking to a few friends in the tech field, I decided to try coding. I love the feeling of accomplishment that comes with learning something new. What I got instead was a completely new feeling.
+                    <br />
+                    <br />
+                    I felt shocked.
+                    <br />
+                    <br />
+                    Coding is rewarding. Coding allows me to create. Coding taught me the truth about a career - it can be incredibly fulfilling once you find the one for you.
+                    <br />
+                    <br />
+                    It was then that I knew I had found a new career path to take.
+                    <br />
+                    <br />
+                    I have recently completed a year-long fellowship at Pursuit where I learned JavaScript, React, HTML, CSS, REST APIs, and test-driven development. I'm excited to bring my creative skills into a role as a Full Stack Web Developer.
                 </p>      
             </div>
         )
